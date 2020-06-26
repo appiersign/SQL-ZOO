773373d6880cbf030179fe51f0ff8aa87898780d
solomon APPIER-SIGN <braasig@gmail.com>
2020-06-26 20:33:52 +0000

    added queries for select name tasks

--- 1_Select_name.sql
+++ 1_Select_name.sql
@@ -1,61 +1,72 @@
-SELECT name, continent, population 
-FROM world;
+SELECT name 
+FROM world
+WHERE name 
+LIKE 'Y%';
 
 SELECT name 
 FROM world
-WHERE population >= 200000000;
-
-SELECT name, (gdp/population) 
-AS per_capita 
-FROM world 
-WHERE population >= 200000000;
-
-SELECT name, population/1000000 
-FROM world 
-WHERE continent = 'South America';
-
-SELECT name, population 
-FROM world 
 WHERE name 
-IN ('France', 'Germany', 'Italy');
+LIKE '%y';
+
+SELECT name 
+FROM world
+WHERE name 
+LIKE '%x%';
+
+SELECT name 
+FROM world
+WHERE name 
+LIKE '%land';
+
+SELECT name 
+FROM world
+WHERE name 
+LIKE 'C%ia';
+
+SELECT name 
+FROM world
+WHERE name 
+LIKE '%oo%';
+
+SELECT name 
+FROM world
+WHERE LENGTH(name) - LENGTH(REPLACE(LOWER(name), 'a', '')) > 2;
+
+SELECT name 
+FROM world
+WHERE name 
+LIKE '_t%'
+ORDER BY name;
+
+SELECT name 
+FROM world
+WHERE name 
+LIKE '%o__o%';
+
+SELECT name 
+FROM world
+WHERE LENGTH(name) 
+LIKE 4;
 
 SELECT name
 FROM world
-WHERE name
-LIKE 'United%';
-
-SELECT name, population, area
-FROM world
-WHERE area > (3000000)
-OR population > 250000000;
-
-SELECT name, population, area
-FROM world
-WHERE area > (3000000)
-XOR population > 250000000;
-
-SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) 
-FROM world
-WHERE continent = 'South America';
-
-SELECT name, ROUND(gdp/population, -3) 
-FROM world 
-WHERE gdp >= 1000000000000;
-
-SELECT name, capital
-FROM world
-WHERE LENGTH(capital) = LENGTH(name);
-
-SELECT name, capital
-FROM world
-WHERE LEFT(name, 1) = LEFT(capital, 1)
-AND name != capital;
+WHERE name = capital;
 
 SELECT name
 FROM world
-WHERE name LIKE '%a%'
-AND name LIKE '%e%'
-AND name LIKE '%i%'
-AND name LIKE '%o%'
-AND name LIKE '%u%'
-AND name NOT LIKE '% %'
+WHERE capital = CONCAT(name, ' City');
+
+SELECT capital, name
+FROM world
+WHERE capital 
+LIKE CONCAT(CONCAT('%', name), '%');
+
+SELECT capital, name
+FROM world
+WHERE REPLACE(capital, name, '') != capital 
+AND LENGTH(name) != LENGTH(capital);
+
+SELECT name, REPLACE(capital, name, '') as ext
+FROM world
+WHERE REPLACE(capital, name, '') != capital 
+AND LENGTH(name) != LENGTH(capital);
